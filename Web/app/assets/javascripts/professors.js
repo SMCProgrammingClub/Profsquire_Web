eea8dcce1f56790ffee78ab1d045ef0ab3c17b46
jer20084 <jer20084@gmail.com>
2015-05-13 18:16:42 -0700

    moved javascript file

---
+++ Web/app/assets/javascripts/professors.js
@@ -0,0 +1,20 @@
+
+var isClicked = true;
+
+$("#professor_full_profile").click(function() {
+    if (isClicked) {
+        $("#professor_profile").show().animate({'height': '600px'});
+        isClicked = false;
+    }else{
+        $("#professor_profile").animate({'height': 0}, 'fast', function(){ $(this).hide(); });
+        isClicked = true;
+    }
+});
+$("#x").click(function() {
+    $("#professor_profile").animate({'height': 0}, 'fast', function(){ $(this).hide(); });
+    isClicked = true;
+});
+$("#close").click(function() {
+    $("#professor_profile").animate({'height': 0}, 'fast', function(){ $(this).hide(); });
+    isClicked = true;
+});
