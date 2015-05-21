a8d9f2ae1a12a3499201289dfbf48f8bf6dab169
Sidney Eubanks <sidneyeubanks@icloud.com>
2015-05-20 20:41:08 -0700

    api updates
    added 3 controllers to the api for attributes, professors, and courses

--- Web/app/controllers/api/professors_controller.rb
+++ Web/app/controllers/api/professors_controller.rb
@@ -1,5 +1,11 @@
 class Api::ProfessorsController < ApplicationController
+
   def index
-    'render json: Professor.all'
+    render json: Professor.all
+  end
+
+  def show
+    professor = Professor.find(params[:id])
+    render json: professor
   end
 end
--- Web/config/routes.rb
+++ Web/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :professors
+    resources :courses
+    resources :attributes
   end
 
   resources :pages
