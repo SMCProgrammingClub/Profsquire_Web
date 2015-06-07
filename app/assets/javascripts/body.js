cfe72a9f614e84057b04fed6baab8e5c730fe38f
Sidney- <onelookingformusic@gmail.com>
2015-06-07 15:47:07 -0700

    6 balls are rolling!
    just need to set them to fade in instead now and possibly delay so they
    each fade in slowly

--- app/assets/javascripts/body.js
+++ app/assets/javascripts/body.js
@@ -2,65 +2,59 @@
  * Created by sidneyeubanks on 6/7/15.
  */
 
+function init() {
+    var canvas = document.getElementsByTagName('canvas')[0];
+    var context = canvas.getContext('2d');
+    var x, y, r, dx, dy;
 
-var context;
-var w = window.innerWidth;//Sets w to the width of the window
-var h = window.innerHeight;//Sets h to the height of the window
-var x=Math.floor((Math.random() * w) + 1);// These set the Coordinates of the starting circle
-var y=Math.floor((Math.random() * h) + 1);// to a random position
-var dx = (Math.random() * (0.20 - 0.01) + 0.01); //These set the speed and the random path
-var dy = (Math.random() * (0.20 - 0.01) + 0.01); //the circles move in
-
-// Set the circle to move in a specific direction of the circles made path
-switch(Math.floor((Math.random() * 3) + 1)) {
-    case 1:
-        dx *= -1;
-        dy *= -1;
-        break;
-    case 2:
-        dx *= -1;
-        break;
-    case 3:
-        dy *= -1;
-        break;
-    default:
-        dx=.15;
-        dy=.15;
-}
-
-// supposed to set the canvas to the size of the window, not working correctly
-function windowsize(){
+    //Sets the width and height of the canvas to fit the window
     var ctx = document.getElementById("canvas");
     ctx.width = window.outerWidth;
     ctx.height = window.innerHeight;
-    //$('#canvas').css('background-color', 'rgba(158, 167, 184, 0.2)');
+
+    var circles = [
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0}];
+
+    for(var i = 0; i < circles.length; i++) {
+        x = Math.floor((Math.random() * window.innerWidth) + 1);// These set the Coordinates of the starting circle
+        y = Math.floor((Math.random() * window.innerHeight) + 1);// to a random position
+        r = Math.floor((Math.random() * 80) + 60);//Sets radius of circle
+        dx = (Math.random() * (0.20 - 0.01) + 0.01); //These set the speed and the random path
+        dy = (Math.random() * (0.20 - 0.01) + 0.01); //the circles move in
+
+        circles[i].y = y;
+        circles[i].x = x;
+        circles[i].r = r;
+        circles[i].vx = dx;
+        circles[i].vy = dy;
+    }
+
+    function draw() {
+
+        context.clearRect(0,0,window.innerWidth,window.innerHeight);
+
+        for(var i = 0; i <circles.length; i++) {
+            context.fillStyle = 'hsl(' + circles[i].color + ',100%,50%)';
+            context.beginPath();
+            context.fillStyle = "rgba(255, 255, 255, 0.15)";
+            context.arc(circles[i].x, circles[i].y, circles[i].r, 0, 2 * Math.PI, false);
+            context.fill();
+
+            if ((circles[i].x + circles[i].vx + circles[i].r > window.innerWidth) ||
+                (circles[i].x - circles[i].r + circles[i].vx < 0)) { circles[i].vx = -circles[i].vx; }
+
+            if ((circles[i].y + circles[i].vy + circles[i].r > window.innerHeight) ||
+                (circles[i].y - circles[i].r + circles[i].vy < 0)) {circles[i].vy = -circles[i].vy;}
+
+            circles[i].x += circles[i].vx;
+            circles[i].y += circles[i].vy;
+        }
+        requestAnimationFrame(draw);    // start
+    }
+    requestAnimationFrame(draw); // loop here
 }
-
-function init() {
-    context = canvas.getContext('2d');
-    windowsize();
-    requestAnimationFrame(draw);    // start, see below
-}
-
-function draw() {
-    context.clearRect(0, 0, w, h);
-    context.beginPath();
-    context.fillStyle = "rgba(255, 255, 255, 0.15)";
-    // Draws a circle of radius 60 at the coordinates x, y on the canvas
-    context.arc(x, y, 60, 0, Math.PI * 2, true);
-    context.fill();
-
-    // Boundary Logic
-    if (x < 0 || x > w) dx = -dx;
-    if (y < 0 || y > h) dy = -dy;
-    x += dx;
-    y += dy;
-
-    requestAnimationFrame(draw);    // loop here
-}
-
-//for (i = 0; i < 6; i++){
-
-//}
-//x=Math.floor((Math.random() * 600) + 1);;
-//y=Math.floor((Math.random() * 600) + 1);;
