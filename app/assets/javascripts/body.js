844f05034467fe2527f0eabb8a13ef7efcc5da64
Sidney- <onelookingformusic@gmail.com>
2015-06-08 21:45:24 -0700

    Design fixes
    Put the profsquire logo in the nav, fixed the Facebook and twitter
    logos, changed their hover colors, and added an extra circle.

--- app/assets/javascripts/body.js
+++ app/assets/javascripts/body.js
@@ -18,6 +18,7 @@ function init() {
         {x: 0, y: 0, r: 0, vx: 0, vy: 0},
         {x: 0, y: 0, r: 0, vx: 0, vy: 0},
         {x: 0, y: 0, r: 0, vx: 0, vy: 0},
+        {x: 0, y: 0, r: 0, vx: 0, vy: 0},
         {x: 0, y: 0, r: 0, vx: 0, vy: 0}];
 
     for(var i = 0; i < circles.length; i++) {//initializes array to 6 random values
